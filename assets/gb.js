2b26cbcfab36f06df3870a4f969f838d6699602e
Chris Dent <chris.dent@gmail.com>
2013-01-27 00:10:17 +0000

    first version of a gardening tool for tiddlyspace
    see: http://cdent.tiddlyspace.com/GardenBone
         http://tiddlyweb.tiddlyspace.com/gardenbone
    
    It's less than correct, but promising.

---
+++ assets/gb.js
@@ -0,0 +1,87 @@
+$(function() {
+	var localBag = tiddlyweb.status.space.name + '_public',
+		allURI = '/bags/' + localBag + '/tiddlers',
+		elderURI = allURI + '?sort=modified;limit=20',
+		alls = {},
+		fronts = {};
+
+	$(document).bind('frontsup', function() {
+		var missings = [],
+			orphans = [];
+
+
+		// calculate the data
+		$.each(fronts, function(key, value) {
+			if (!alls[key]) {
+				missings.push(key);
+			}
+		});
+		$.each(alls, function(key, value) {
+			if (!fronts[key]) {
+				orphans.push(key);
+			}
+		});
+
+		// update the display
+		$('#orphans').empty();
+		$.each(orphans, function(index, title) {
+			var link = $('<a>').attr('href', '/' + encodeURIComponent(title))
+				.text(title);
+			var list = $('<li>').append(link);
+			$('#orphans').append(list);
+		});
+		$('#missing').empty();
+		$.each(missings, function(index, title) {
+			var link = $('<a>').attr('href', '/' + encodeURIComponent(title))
+				.text(title);
+			var list = $('<li>').append(link);
+			$('#missing').append(list);
+		});
+	});
+
+	function startFront(tiddler) {
+		links(tiddler, 'frontlinks', addFront);
+	}
+
+	function addFront(tiddlers) {
+		$.each(tiddlers, function(index, tiddler) {
+			if (tiddler.bag === localBag) {
+				fronts[tiddler.title] = 1;
+			}
+		});
+		$(document).trigger('frontsup');
+	}
+
+	function links(tiddler, path, updater) {
+		$.ajax({
+			dataType: 'json',
+			url: tiddler.uri + '/' + path,
+			success: updater
+		});
+	}
+
+	$.ajax({
+		dataType: 'json',
+		url: allURI,
+		success: function(tiddlers) {
+			$.each(tiddlers, function(index, tiddler) {
+				alls[tiddler.title] = 1;
+				startFront(tiddler);
+			});
+		}
+	});
+
+	$.ajax({
+		dataType: 'json',
+		url: elderURI,
+		success: function(tiddlers) {
+			$('#elders').empty();
+			$.each(tiddlers, function(index, tiddler) {
+				var link = $('<a>').attr('href', tiddler.uri).text(tiddler.title);
+				var list = $('<li>').append(link);
+				$('#elders').append(list);
+			});
+		}
+	});
+
+});
